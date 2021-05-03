e24ec96c6ab9ae3fb36a6636b544b4840c818ca6
Alessandro <Alessandro@LAPTOP-2Q5TD8MO.station>
2021-05-03 09:47:45 +0200

    visita in ampiezza e profondità

--- src/main/java/it/polito/tdp/metroparis/model/Model.java
+++ src/main/java/it/polito/tdp/metroparis/model/Model.java
@@ -1,11 +1,15 @@
 package it.polito.tdp.metroparis.model;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import org.jgrapht.Graph;
 import org.jgrapht.Graphs;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.SimpleGraph;
+import org.jgrapht.traverse.BreadthFirstIterator;
+import org.jgrapht.traverse.DepthFirstIterator;
 
 import it.polito.tdp.metroparis.db.MetroDAO;
 
@@ -40,7 +44,74 @@ public class Model {
 		for(Connessione c: connessioni) {
 			this.grafo.addEdge(c.getStazA(), c.getStazP());
 		}
+		
+		System.out.format("Grafo creato con %d vertici e %d archi\n",
+				this.grafo.vertexSet().size(), this.grafo.edgeSet().size()) ;
+		
 		//System.out.println(this.grafo);
+		
+		
+		//COME FARE PER TROVARE I VERTICI VICINI AD UN DETERMINATO VERTICE?
+//		Fermata f=null;
+		/*
+		Set<DefaultEdge> archi = this.grafo.edgesOf(f);
+		for(DefaultEdge e: archi) {
+			/*Fermata f1 = this.grafo.getEdgeSource(e);
+			//oppure
+			Fermata f2 = this.grafo.getEdgeTarget(e);
+			if(f1.equals(f)) {
+				//f2 è quello che mi serve
+			}else {
+				//f1 è quello che miserve
+			}
+			
+			//questo comando fa la stessa cosa
+			
+			Fermata f1 = Graphs.getOppositeVertex(this.grafo, e, f);
+		} */
+		
+		//altro comando uguale
+//		List<Fermata> fermateAdiacenti = Graphs.successorListOf(grafo, f);  //con questo non ci interessano più gli archi
+		//E SE VOGLIO L'ELENCO DI VERTICI PRECEDENTI? CIOè ENTRANTI?
+//		List<Fermata> fermateAdiacent = Graphs.predecessorListOf(grafo, f);
+		//bel caso di grafo bidirezionale uso Graphs.outgoingEdgesOf()
+		
+		
+		
+	}
+	
+	//voglio trovare tutti vertci raggiungibili da un certo vertice
+	//facciamo visita in ampiezza
+	public List<Fermata> fermateRaggiungibili(Fermata partenza){
+		BreadthFirstIterator<Fermata, DefaultEdge> bfv = new BreadthFirstIterator<>(this.grafo, partenza);
+		List <Fermata> result = new ArrayList<>();
+		
+		while(bfv.hasNext()) {  //ogni volta che hasNext è vero vuol dire che ci sono altri vertici da scorprire
+			Fermata f = bfv.next();
+			result.add(f);
+		}
+		return result;
+	}
+	
+	//facciamo visita in profondità
+	public List<Fermata> fermateRaggiungibili2(Fermata partenza){
+		DepthFirstIterator<Fermata, DefaultEdge> dfv = new DepthFirstIterator<>(this.grafo, partenza);
+		List <Fermata> result = new ArrayList<>();
+		
+		while(dfv.hasNext()) {  //ogni volta che hasNext è vero vuol dire che ci sono altri vertici da scorprire
+			Fermata f = dfv.next();
+			result.add(f);
+		}
+		return result;
+	}
+	
+	public Fermata trovaFermata(String nome) {
+		for(Fermata f: this.grafo.vertexSet()) {
+			if(f.getNome().equals(nome)) {
+				return f;
+			}
+		}
+		return null;
 	}
 
 }
--- src/main/java/it/polito/tdp/metroparis/model/TestModel.java
+++ src/main/java/it/polito/tdp/metroparis/model/TestModel.java
@@ -1,11 +1,23 @@
 package it.polito.tdp.metroparis.model;
 
+import java.util.List;
+
 public class TestModel {
 
 	public static void main(String[] args) {
 		Model m = new Model();
 		
 		m.creaGrafo();
+		
+		Fermata p = m.trovaFermata("La Fourche");
+		if(p==null) {
+			System.out.println("Fermata non trovata");
+		}else {
+		List<Fermata> raggiungibili = m.fermateRaggiungibili(p);
+		List<Fermata> raggiungibili2 = m.fermateRaggiungibili2(p);
+		System.out.println(raggiungibili);
+		System.out.println(raggiungibili2);
+		}
 
 	}
 
